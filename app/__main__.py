4c29f3a26b92b541cf4de887b06955df00b0d553
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-10 02:28:18 +0500

    chore: add application entrypoints

---
+++ app/__main__.py
@@ -0,0 +1,8 @@
+def main() -> None:
+    """Application entrypoint."""
+    print("Application bootstrap completed")
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/runners.py
@@ -0,0 +1,3 @@
+def run_polling() -> None:
+    """Start bot polling."""
+    pass

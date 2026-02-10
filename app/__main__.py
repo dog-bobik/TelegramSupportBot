c5f2d99c95107195c8fa10ebb6de4b32d6edeaec
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-10 23:15:55 +0500

    feat(app): initialize app logger on startup

--- app/__main__.py
+++ app/__main__.py
@@ -4,11 +4,15 @@ Application entrypoint.
 
 from app.factory import create_app_config
 from app.models.config.env import AppConfig
+from app.utils.logging import setup_logging, AppLogger
+
+logger = AppLogger(name=__name__)
 
 
 def main() -> None:
+    setup_logging()
     config: AppConfig = create_app_config()
-    print("Application configuration initialized")
+    logger.info("Application configuration initialized.")
 
 
 if __name__ == "__main__":

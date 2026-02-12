251e0b87011348cd0252dcd4ebfc90c736e76e24
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-13 01:41:59 +0500

    feat(runner): add bot runner

--- app/runners.py
+++ app/runners.py
@@ -1,3 +1,24 @@
-def run_polling() -> None:
-    """Start bot polling."""
-    pass
+from __future__ import annotations
+from aiogram import Bot, Dispatcher
+
+from app.models.config import AppConfig
+
+
+async def polling_startup(bots: list[Bot], config: AppConfig) -> None:
+    """
+    Clean webhook before starting polling mode.
+    """
+
+    for bot in bots:
+        await bot.delete_webhook(
+            drop_pending_updates=config.telegram.drop_pending_updates
+        )
+
+
+def run_polling(dispatcher: Dispatcher, bot: Bot) -> None:
+    """
+    Configure and launch bot in polling mode.
+    """
+
+    dispatcher.startup.register(polling_startup)
+    return dispatcher.run_polling(bot)

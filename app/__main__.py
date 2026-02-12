6c3cf8e03ee2bfd07a8ec98f014cb71895a1695f
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-13 01:45:50 +0500

    feat(entry): setup bot startup

--- app/__main__.py
+++ app/__main__.py
@@ -1,18 +1,23 @@
-"""
-Application entrypoint.
-"""
-
-from app.factory import create_app_config
+from app.factory import create_app_config, create_dispatcher, create_bot
+from aiogram import Dispatcher, Bot
 from app.models.config.env import AppConfig
 from app.utils.logging import setup_logging, AppLogger
+from .runners import run_polling
 
 logger = AppLogger(name=__name__)
 
 
 def main() -> None:
+    """
+    Initialize and start the application.
+    """
+
     setup_logging()
     config: AppConfig = create_app_config()
+    dispatcher: Dispatcher = create_dispatcher(config=config)
+    bot: Bot = create_bot(config=config)
     logger.info("Application configuration initialized.")
+    return run_polling(dispatcher=dispatcher, bot=bot)
 
 
 if __name__ == "__main__":
--- app/factory/__init__.py
+++ app/factory/__init__.py
@@ -1,4 +1,10 @@
 from .app_config import create_app_config
 from .session_pool import create_session_pool
+from .telegram import create_bot, create_dispatcher
 
-__all__: list[str] = ["create_app_config", "create_session_pool"]
+__all__ = [
+    "create_app_config",
+    "create_session_pool",
+    "create_bot",
+    "create_dispatcher",
+]
